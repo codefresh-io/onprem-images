b0e73ffc6abb54a47a98e37fd9ba4be201a358b5
kosta709 <kosta@deepcoding.ai>
2019-11-17 13:38:33 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Images for Codefresh Onpremises intallations
+
+### Prerequesites:
+setup docker and `docker login to both source and destination registries`
+
+to login with obtained Google Service Accounbt for Codefresh Enterprise registery:
+```
+docker login -u oauth2accesstoken -p "$(cat sa.json)" https://gcr.io
+```
+
+### Push required images from Codefresh and public registry to private
+```
+push-to-registry.sh <private-registry-addr> <release-name>
+```
---
+++ push-to-registry.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+#
+usage() {
+  echo "Usage:
+  $0 <private-registry-addr> <release-name>
+  
+  Prerequisite: docker login to both source and destination registry
+  "
+}
+
+DIR=$(dirname $0)
+
+PRIVATE_REGISTRY_ADDR=$1
+RELEASE_NAME=$2
+
+if [[ -z "${PRIVATE_REGISTRY_ADDR}" || -z "${RELEASE_NAME}" ]]; then
+  usage
+  exit 1
+fi
+
+IMAGES_LIST=${DIR}/releases/images-list-${RELEASE_NAME}
+if [[ ! -f "${IMAGES_LIST}" ]]; then
+  usage
+  echo "ERROR: cannot file images list in ${IMAGES_LIST} "
+  exit 1
+fi
+
+echo "Starting at $(date)"
+echo "Processing images list $IMAGES_LIST"
+
+if [[ -n "${DRY_RUN}" ]]; then
+  echo "DRY_RUN MODE"
+  DOCKER="echo docker"
+else
+  DOCKER="docker"
+fi
+
+# there are 3 types of image names:
+# 1. non-codefresh like "bitnami/mongo:123" - convert to "private-registry-addr/bitnami/mongo:123"
+# 2. codefresh public images like "codefresh/engine:123" - convert to "private-registry-addr/codefresh/mongo:123"
+# 3. codefresh private images like gcr.io/codefresh-enterprise/codefresh/cf-api:cf-onprem-v1.0.86 - will be convert to "private-registry-addr/codefresh/mongo:123
+DELIMITER='codefresh/'
+cat ${IMAGES_LIST} | while read line
+do
+  PULL_IMAGE=$line
+  PUSH_NAME=$(echo $PULL_IMAGE | awk -F"${DELIMITER}" -vPRIVATE_REGISTRY_ADDR=${PRIVATE_REGISTRY_ADDR} \
+      '{if($2 == ""){print PRIVATE_REGISTRY_ADDR"/"$1}  else {print PRIVATE_REGISTRY_ADDR"/codefresh/"$2}}')
+  echo "$PULL_IMAGE    ->    $PUSH_NAME "
+
+  PULL_COMMAND="$DOCKER pull $PULL_IMAGE"
+  TAG_COMMAND="$DOCKER tag $PULL $PUSH_IMAGE"
+  PUSH_COMMAND="$DOCKER push $PUSH_IMAGE"
+
+  echo "---------- Migrate $PULL to $PUSH"
+  eval $PULL_COMMAND && echo -e "Pull $PULL_IMAGE completed - $(date) !!!\n" && \
+  eval $TAG_COMMAND && echo -e "Tag $PUSH_IMAGE completed - $(date) !!!\n" && \
+  eval $PUSH_COMMAND && echo -e "Push $PUSH_IMAGE completed - $(date) !!!\n"
+
+  if [[ $? == 0 ]]; then
+    echo "$PUSH" >> $DONE_FILE
+  else
+    echo "ERROR - $PULL to $PUSH"
+  fi
+  
+done
